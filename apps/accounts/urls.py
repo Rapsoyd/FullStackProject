cd54e1c11e312da75ce8cf2ebe08e9e17d461c85
Egor <shamber.egor@mail.ru>
2024-05-25 22:44:14 +0400

    Login and Sign UP

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import ProfileUpdateView, ProfileDetailView
+from .views import ProfileUpdateView, ProfileDetailView, register, login, logout
 
 urlpatterns = [
     path("user/edit", ProfileUpdateView.as_view(), name='profile_edit'),
-    path("user/<str:slug>/", ProfileDetailView.as_view(), name='profile_detail')
+    path("user/<str:slug>/", ProfileDetailView.as_view(), name='profile_detail'),
+    path("user/register", register, name='register'),
+    path("user/login", login, name='login'),
+    path("user/logout", logout, name='logout'),
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,9 +1,49 @@
 from django.views.generic import DetailView, UpdateView
 from django.db import transaction
 from django.urls import reverse_lazy
-
+from .forms import UserRegisterForm, UserLoginForm
 from .models import Profile
 from .forms import UserUpdateForm, ProfileUpdateForm
+from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.error(request, 'Неверное имя пользователя или пароль.')
+    else:
+        form = UserLoginForm()
+    return render(request, 'accounts/login.html', {'form': form})
+
+
+@login_required
+def logout(request):
+    logout(request)
+    messages.success(request, 'Вы вышли из системы.')
+    return redirect('home')  # Замените 'home' на имя вашей домашней страницы
 
 
 class ProfileDetailView(DetailView):
@@ -54,5 +94,3 @@ class ProfileUpdateView(UpdateView):
 
     def get_success_url(self):
         return reverse_lazy('profile_detail', kwargs={'slug': self.object.slug})
-
-
