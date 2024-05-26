1087056bdece49ac9418d2f40f750d1788b15077
dudarph <danelutrekov@gmail.com>
2024-05-26 17:41:19 +0400

    Начали делать формы

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -6,12 +6,23 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 UPDATE_FORM_WIDGET = forms.TextInput(attrs={'class': 'form-control mb-1'})
 
 
-class UserRegisterForm(UserCreationForm):
+class UserRegisterForm(forms.ModelForm):
     email = forms.EmailField()
+    username = forms.CharField(max_length=30)
+    password1 = forms.CharField(widget=forms.PasswordInput, label="Введите ваш пароль")
+    password2 = forms.CharField(widget=forms.PasswordInput, label="Введите ваш пароль ещё раз")
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+    def clean_password2(self):
+        password1 = self.cleaned_data['password1']
+        password2 = self.cleaned_data['password2']
+
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError('Пароли не совпадают')
+        return password2
 
 
 class UserLoginForm(AuthenticationForm):
