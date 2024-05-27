79e7f146dc260f8a71a6014ef9f51993a612bd0a
Egor <shamber.egor@mail.ru>
2024-05-27 17:18:45 +0400

    WEBP convertor

--- apps/accounts/signals.py
+++ apps/accounts/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from apps.accounts.models import Profile
@@ -8,4 +8,15 @@ from apps.accounts.models import Profile
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)   
+        Profile.objects.create(user=instance)
+
+
+@receiver(pre_save, sender=Profile)
+def delete_previous_avatar(sender, instance, **kwargs):
+    if instance.pk:
+        try:
+            old_avatar = sender.objects.get(pk=instance.pk).avatar
+            if old_avatar != instance.avatar:
+                old_avatar.delete(save=False)
+        except sender.DoesNotExist:
+            pass
