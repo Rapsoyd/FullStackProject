303773752a52a19d4dabb6e7fdaf89f97a462fb4
Egor <shamber.egor@mail.ru>
2024-05-27 14:03:57 +0400

    Nice title

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,13 +1,17 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView  # Импортируем базовое представление
 from apps.blog.models import Post, Category
 from .forms import PostCreateForm, PostUpdateForm
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
 
 
 class PostListView(ListView):
     model = Post
-    template_name = 'blog/post_list.html'
+    template_name = 'blog/blogs.html'
     context_object_name = "posts"
-    extra_context = {"title": "Главная страница"}  # Получаем заданный контекст не из бд
     paginate_by = 2
     queryset = Post.custom.all()
 
@@ -22,14 +26,9 @@ class PostDetailView(DetailView):
     template_name = 'blog/post_detail.html'
     context_object_name = "post"  # Переменная с которой мы будем работать в шаблоне
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["title"] = self.object.title
-        return context
-
 
 class PostFromCategory(ListView):
-    template_name = "blog/post_list.html"
+    template_name = "blog/blogs.html"
     context_object_name = "posts"
     category = None
     paginate_by = 2
@@ -44,17 +43,13 @@ class PostFromCategory(ListView):
             queryset = Post.objects.filter(category__in=sub_cat)
         return queryset
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["title"] = f"Записи из категории: {self.category.title}"
-        return context
+
 
 
 class PostCreateView(CreateView):
     model = Post
     template_name = "blog/post_create.html"
     form_class = PostCreateForm
-    extra_context = {"title": "Добавление статьи на сайт"}
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -71,10 +66,6 @@ class PostUpdateView(UpdateView):
     context_object_name = 'post'
     form_class = PostUpdateForm
 
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = f'Обновление статьи: {self.object.title}'
-        return context
 
     def form_valid(self, form):
         form.instance.updater = self.request.user
