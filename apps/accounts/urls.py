9ef4b2a3929d059a1b52471bd305be8a43a7ce83
dudarph <danelutrekov@gmail.com>
2024-06-03 20:25:13 +0400

    Вроде замерджил, был какой-то error c db.sqlite

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
-from .views import ProfileUpdateView, ProfileDetailView, register_user, login_user, logout_user
+from .views import (ProfileUpdateView, ProfileDetailView,
+                    register_user, login_user, logout_user,
+                    password_reset_request, password_reset_confirm,
+                    reset_done)
+
 
 urlpatterns = [
+    path("user/reset_done", reset_done, name='password_reset_done'),
     path("user/edit", ProfileUpdateView.as_view(), name='profile_edit'),
     path("user/<str:slug>/", ProfileDetailView.as_view(), name='profile_detail'),
     path("user/register", register_user, name='register'),
     path("user/login", login_user, name='login'),
     path("user/logout", logout_user, name='logout'),
+    path("user/reset", password_reset_request, name='password_reset_request'),
+    path("user/reset_confirm/<uidb64>/<token>/", password_reset_confirm, name='password_reset_confirm'),
 ]
