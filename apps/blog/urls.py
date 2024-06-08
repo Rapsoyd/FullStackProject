0e58442f12117968cc62b71b10913ac33f3b2064
Egor <shamber.egor@mail.ru>
2024-06-08 18:20:17 +0400

    Function added

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostFromCategory, post_create_view, PostUpdateView, index
+from .views import user_post_list, PostListView, PostDetailView, PostFromCategory, post_create_view, PostUpdateView, index
 
 urlpatterns = [
     path("", index, name='index'),
     path("blogs/", PostListView.as_view(), name="blogs_page"),
     path('post/create/', post_create_view, name='post_create'),
+    path('post/user_posts', user_post_list, name='user_posts'),
     path('post/<str:slug>/update/', PostUpdateView.as_view(), name='post_update'),
     path("post/<str:slug>/", PostDetailView.as_view(), name="post_detail"),
     path("category/<str:slug>/", PostFromCategory.as_view(), name="post_by_category")
